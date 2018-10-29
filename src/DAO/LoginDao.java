8383c42bdb72d8e1a5b8fb73bf8fa68886ed692b
marins <tiagomarinsq97@gmail.com>
2018-10-29 20:12:16 -0300

    login funcionando caraio \o/

--- src/DAO/LoginDao.java
+++ src/DAO/LoginDao.java
@@ -2,50 +2,33 @@ package DAO;
 
 import javax.persistence.Query;
 
-import model.Morador;
-import model.Porteiro;
-import model.Proprietario;
+import model.Usuario;
 
 @SuppressWarnings("rawtypes")
 public class LoginDao extends DAO{
 	
-	public Object findByLoginAndSenha(String login, String senha) {
+	public Usuario findByLoginAndSenha(String login, String senha) {
 		Object retorno = null;
 		try {
 			manager.clear();
-			Query query = manager.createQuery("Select m from Morador m WHERE m.login = :login and m.senha = :senha");
+			Query query = manager.createQuery("Select u from Usuario u WHERE u.login = :login and u.senha = :senha");
 			query.setParameter("login", login);
 			query.setParameter("senha", senha);
 			query.setMaxResults(1);
 			retorno = query.getSingleResult();
 			if(retorno != null) {
-				return (Morador) retorno;
+				return (Usuario) retorno;
 			}else{
-				manager.clear(); //limpeza de cache de queries
-				query = manager.createQuery("Select p from Porteiro p WHERE p.login = :login and p.senha = :senha");
-				query.setParameter("login", login);
-				query.setParameter("senha", senha);
-				query.setMaxResults(1);
-				retorno = query.getSingleResult();
-			if(retorno != null) {
-				return (Porteiro) retorno;
-			}else{
-				manager.clear(); //limpeza de cache de queries
-				query = manager.createQuery("Select p from Proprietario p WHERE p.login = :login and p.senha = :senha");
-				query.setParameter("login", login);
-				query.setParameter("senha", senha);
-				query.setMaxResults(1);
-				retorno = query.getSingleResult();
-			if(retorno != null) {
-					return (Proprietario) retorno;
-			}else{
+				System.out.println("Errou!");
 					return null;
 			}
-				}
-					
-			}
+			
+		
 		}catch (Exception e) {
+			e.printStackTrace();
 			return null;
 		}
+
 	}
+	
 }
--- src/control/LoginControl.java
+++ src/control/LoginControl.java
@@ -1,12 +1,23 @@
 package control;
 
-import DAO.LoginDao;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
+import DAO.LoginDao;
+import model.Usuario;
+
+@Path("login")
 public class LoginControl {
 
 	private LoginDao loginDao = new LoginDao();
 	
-	public Object validarLogin(String login, String senha) {
+	@GET
+	@Path("validar")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Usuario validarLogin(@QueryParam("login") String login, @QueryParam("senha") String senha) {
 		return loginDao.findByLoginAndSenha(login, senha);
 	}
 	
