3a29af12a41612ae0107f05cb9643e6a7971bf39
oliveirajoabe <oliveira_joabe@hotmail.com>
2018-11-18 15:51:08 -0200

    .

--- src/DAO/PorteiroDao.java
+++ src/DAO/PorteiroDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.Query;
 
 import model.Porteiro;
+import model.Usuario;
 
 @SuppressWarnings("rawtypes")
 public class PorteiroDao extends DAO{
@@ -16,6 +17,32 @@ public class PorteiroDao extends DAO{
 		query.setMaxResults(limitePorPagina);
 		query.setFirstResult(pagina*limitePorPagina-limitePorPagina);
 		return query.getResultList();
+	}
+	
+	public Usuario findByNome(String nome) {
+		Object retorno = null;
+		try {
+			manager.clear();
+			Query query = manager.createQuery("Select u from Usuario u WHERE ("
+					+ "lower(u.nome) like '%'||lower( coalesce(:nome, '') )||'%' OR "
+					+ " coalesce(:nome, '') = ''"
+					+ ") ");
+			query.setParameter("nome", nome);
+			query.setMaxResults(1);
+			retorno = query.getSingleResult();
+			if(retorno != null) {
+				return (Usuario) retorno;
+			}else{
+				System.out.println("Errou!");
+					return null;
+			}
+			
+		
+		}catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 
 }
